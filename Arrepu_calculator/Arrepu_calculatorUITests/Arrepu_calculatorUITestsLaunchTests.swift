a1636ae7c73481c35872a1ae905ddf8cd524d611
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2022-02-14 20:16:54 -0600

    Add files via upload

---
+++ Arrepu_calculator/Arrepu_calculatorUITests/Arrepu_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Arrepu_calculatorUITestsLaunchTests.swift
+//  Arrepu_calculatorUITests
+//
+//  Created by Arrepu,Sarath on 2/14/22.
+//
+
+import XCTest
+
+class Arrepu_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
